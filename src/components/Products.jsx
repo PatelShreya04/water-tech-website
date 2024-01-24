37b81b227a254d7f2eb387ea298f89a1642a7cae
Dharmi <dharmip060@gmail.com>
2024-01-24 19:50:21 +0530

    Routing problem

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -50,15 +50,15 @@ const Products = () => {
 
         <div className="flex md:flex-row flex-col justify-center md:gap-12 gap-10 pl-10 pr-10">
           {/* Cartridge Product */}
-          <a href="/uffilters" className="product-card">
+          <Link to="/uffilters" className="product-card">
           <img
     src={uf}
     alt="Cartridge"
     className="object-cover w-72 h-auto shadow-lg shadow-gray-600 border-5 border-solid border-gray-300 transform transition-transform duration-300 ease-out hover:scale-110"
     style={{ borderRadius: '8px' }}// Optional: Add border-radius for rounded corners
   />
-            <p className="mt-2 text-center text-primary font-bold   md:text-2xl lg:text-[22px] text-[20px] transform hover:scale-125 transition-transform duration-300 ease-out">UF Filters</p>
-          </a>
+            <a href="/uffilters" className="mt-2 text-center text-primary font-bold   md:text-2xl lg:text-[22px] text-[20px] transform hover:scale-125 transition-transform duration-300 ease-out">UF Filters</a>
+          </Link>
 
           {/* Filters Product */}
           <a href="/filters" className="product-card">
