c1950292cac6a1c43540af8ca67c478c5ce5d3a7
Dharmi <dharmip060@gmail.com>
2024-01-24 20:14:27 +0530

    Linking error solved hopefully!!

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -50,7 +50,7 @@ const Products = () => {
 
         <div className="flex md:flex-row flex-col justify-center md:gap-12 gap-10 pl-10 pr-10">
           {/* Cartridge Product */}
-          <Link to="/uffilters" className="product-card">
+          <a href="/uffilters" className="product-card">
           <img
     src={uf}
     alt="Cartridge"
@@ -58,21 +58,21 @@ const Products = () => {
     style={{ borderRadius: '8px' }}// Optional: Add border-radius for rounded corners
   />
             <p  className="mt-2 text-center text-primary font-bold   md:text-2xl lg:text-[22px] text-[20px] transform hover:scale-125 transition-transform duration-300 ease-out">UF Filters</p>
-          </Link>
+          </a>
 
           {/* Filters Product */}
-          <Link to="/filters" className="product-card">
+          <a href="/filters" className="product-card">
             <img src={copper1} alt="Filters" className="object-cover w-72 h-auto shadow-lg shadow-gray-600 border-5 border-solid border-gray-300 transform transition-transform duration-300 ease-out hover:scale-110"
     style={{ borderRadius: '8px' }} />
             <p className="mt-2 text-center text-primary font-bold  md:text-2xl lg:text-[22px] text-[20px]transform hover:scale-125 transition-transform duration-300 ease-out ">Copper Alkaline Filters</p>
-          </Link>
+          </a>
 
           {/* Membrane Product */}
-          <Link to="/skid" className="product-card">
+          <a href="/skid" className="product-card">
             <img src={skid1} alt="Membrane" className="object-cover w-72 h-auto shadow-lg shadow-gray-600 border-5 border-solid border-gray-300 transform transition-transform duration-300 ease-out hover:scale-110"
     style={{ borderRadius: '8px' }} />
             <p className="mt-2 text-center text-primary font-bold   md:text-2xl lg:text-[22px] text-[20px] transform hover:scale-125 transition-transform duration-300 ease-out">Skid</p>
-          </Link>
+          </a>
 
         
           
