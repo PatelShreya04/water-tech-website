8d2a7144d155ffa92b41b22a7074638c07e585cd
Dharmi <dharmip060@gmail.com>
2024-01-30 23:23:43 +0530

    home page 1

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -37,7 +37,7 @@ const Slider = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 1.5, ease: 'easeInOut' }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
       >
         <img
           key={currentIndex}
