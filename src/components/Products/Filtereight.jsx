b49c78265662d520b20e9699602dec2fa41831cc
Dharmi <dharmip060@gmail.com>
2024-02-06 10:36:42 +0530

    Extra products removed

--- src/components/Products/Filtereight.jsx
+++ src/components/Products/Filtereight.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import image1 from '../../assets/8ff/8filter.jpg';
 import image2 from '../../assets/8ff/8filter2.jpg'
-import image3 from '../../assets/8ff/8filter3.jpg'
-import image4 from '../../assets/8ff/8filter4.png';
-import image5 from '../../assets/8ff/8filter5.jpg';
-import image6 from '../../assets/8ff/copper-alk-filter-2.jpg'
-import image7 from '../../assets/8ff/copperalkaline8.png';
+// import image3 from '../../assets/8ff/8filter3.jpg'
+// import image4 from '../../assets/8ff/8filter4.png';
+// import image5 from '../../assets/8ff/8filter5.jpg';
+// import image6 from '../../assets/8ff/copper-alk-filter-2.jpg'
+// import image7 from '../../assets/8ff/copperalkaline8.png';
 import image8 from '../../assets/8ff/mineral-2.jpg'
 import image9 from '../../assets/8ff/mineral-cartridge.jpg'
 import Nav from '../Nav';
@@ -13,14 +13,14 @@ import Footer from '../Footer';
 import Header from '../Header';
 const Filtereight = () => {
     const filterImages = [
-        image1, image2, image3, image4, image5, image6, image7, image8, image9
+        image1, image2, image8, image9
       ];
     
       return (
         <div>
             <Header />
             <Nav/>
-            <h2 className='text-5xl font-bold mb-6 text-primary text-center custom-text-shadow transform hover:scale-125 transition-transform duration-300 ease-out '>Filters</h2>
+            <h2 className='text-5xl font-bold mb-6 text-primary text-center custom-text-shadow transform hover:scale-125 transition-transform duration-300 ease-out '>8'' Filters</h2>
     
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 pl-10 pr-10 md:mb-20 mb-10">
           {filterImages.map((image, index) => (
--- src/components/Products/Filterten.jsx
+++ src/components/Products/Filterten.jsx
@@ -3,8 +3,8 @@ import image1 from '../../assets/10ff/10filter.jpg'
 import image2 from '../../assets/10ff/alkalinefilter10.png'
 import image3 from '../../assets/10ff/alkalinefilter20.png'
 import image4 from '../../assets/10ff/ca10.png'
-import image5 from '../../assets/10ff/copper-alk-filter4.jpg'
-import image6 from '../../assets/10ff/copperalkaline10.png'
+// import image5 from '../../assets/10ff/copper-alk-filter4.jpg'
+// import image6 from '../../assets/10ff/copperalkaline10.png'
 import image7 from '../../assets/10ff/filterscollage.jpg'
 import image8 from '../../assets/10ff/membrane-1.jpg'
 import Nav from '../Nav'
@@ -13,14 +13,14 @@ import Header from '../Header'
 
 const Filterten = () => {
     const filterImages = [
-        image1, image2, image3, image4, image5, image6, image7, image8
+        image1, image2, image3, image4, image7, image8
       ];
     
       return (
         <div>
             <Header />
             <Nav />
-            <h2 className='text-5xl font-bold mb-6 text-primary text-center custom-text-shadow transform hover:scale-125 transition-transform duration-300 ease-out '>Filters</h2>
+            <h2 className='text-5xl font-bold mb-6 text-primary text-center custom-text-shadow transform hover:scale-125 transition-transform duration-300 ease-out '>10'' Filters</h2>
     
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 pl-10 pr-10 md:mb-20 mb-10">
           {filterImages.map((image, index) => (
